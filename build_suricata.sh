28e09cba9cb0ee522500f44b364ba0a9054f2ded
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-11-22 09:58:42 +0000

    Improve fpm packaging command
    Add package dependencies and post install action

--- build_suricata.sh
+++ build_suricata.sh
@@ -1,4 +1,5 @@
 cd /tmp
+gem install fpm
 wget http://www.openinfosecfoundation.org/download/suricata-3.1.tar.gz
 tar xzf suricata-3.1.tar.gz
 cd suricata-3.1
@@ -6,4 +7,4 @@ cd suricata-3.1
 make
 make install-full DESTDIR=/tmp/installdir
 cd /tmp/destdir
-# fpm -s dir -t rpm -n suricata -v 3.1 -C /tmp/installdir -p suricata-VERSION_ARCH.rpm usr/ etc/
+fpm -s dir -t rpm -f -n suricata -v 3.1 -a x86_64 -C /tmp/installdir --template-scripts --after-install /tmp/destdir/postinstall -d 'libpcap > 1.5.2' -d 'libnet > 1.1.5' -d 'libnetfilter_queue > 1.0.1' -d 'jansson > 2.3' -d 'libyaml > 0.1.3' -p suricata-VERSION_ARCH.rpm usr/ etc/
